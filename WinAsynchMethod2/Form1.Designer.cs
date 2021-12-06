bbbbd59292a7ee71fbb01226febfdd59bf6a9de6
yyyeeer666@outlook.com <yyyeeer666@outlook.com>
2021-12-06 17:54:30 +0300

    Выполнил задание 3.2

--- WinAsynchMethod2/Form1.Designer.cs
+++ WinAsynchMethod2/Form1.Designer.cs
@@ -36,7 +36,6 @@ namespace WinAsynchMethod
             this.txbA = new System.Windows.Forms.TextBox();
             this.txbB = new System.Windows.Forms.TextBox();
             this.helpProvider1 = new System.Windows.Forms.HelpProvider();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnRun
@@ -95,22 +94,11 @@ namespace WinAsynchMethod
             // 
             this.helpProvider1.HelpNamespace = "C:\\Users\\1\\Desktop\\Praktika\\WinAsynchMethod2\\Справка.docx";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(321, 128);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Справка";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(409, 161);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.txbB);
             this.Controls.Add(this.txbA);
             this.Controls.Add(this.lblB);
@@ -133,7 +121,6 @@ namespace WinAsynchMethod
         private System.Windows.Forms.TextBox txbA;
         private System.Windows.Forms.TextBox txbB;
         private System.Windows.Forms.HelpProvider helpProvider1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
