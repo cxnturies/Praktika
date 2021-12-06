40fbb32b13c32e8db9d67a1c1de475bcfe13c162
yyyeeer666@outlook.com <yyyeeer666@outlook.com>
2021-12-06 15:03:06 +0300

    Выполнил задание 2.3

--- WinAsynchMethod/Form1.cs
+++ WinAsynchMethod/Form1.cs
@@ -80,6 +80,7 @@ namespace WinAsynchMethod
             summdelegate.BeginInvoke(a, b, cb, summdelegate);
         }
 
+
         private void btnWork_Click_1(object sender, EventArgs e)
         {
             MessageBox.Show("Работа кипит!!!");
