e2eded14797f4a392507358d730ac29c1249f4cf
yyyeeer666@outlook.com <yyyeeer666@outlook.com>
2021-12-07 12:06:21 +0300

    Добавлены oleDbCommand1 и oleDbConnection1 на форму

---
+++ WinBD/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace WinBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.oleDbCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.oleDbConnection1 = new System.Data.OleDb.OleDbConnection();
+            this.SuspendLayout();
+            // 
+            // oleDbCommand1
+            // 
+            this.oleDbCommand1.CommandText = "SELECT * FROM Поставщики";
+            this.oleDbCommand1.Connection = this.oleDbConnection1;
+            // 
+            // oleDbConnection1
+            // 
+            this.oleDbConnection1.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Users\\1\\Desktop\\Praktika\\WinBD\\RB" +
+    "Product.mdb";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Data.OleDb.OleDbCommand oleDbCommand1;
+        private System.Data.OleDb.OleDbConnection oleDbConnection1;
+    }
+}
+
