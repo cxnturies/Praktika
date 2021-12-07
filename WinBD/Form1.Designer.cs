7a3a15c883a4a877da0055683ce76320ca9dd520
yyyeeer666@outlook.com <yyyeeer666@outlook.com>
2021-12-07 12:15:26 +0300

    Добавлен обработчик нажатия кнопки

--- WinBD/Form1.Designer.cs
+++ WinBD/Form1.Designer.cs
@@ -53,6 +53,7 @@ namespace WinBD
             this.button1.TabIndex = 0;
             this.button1.Text = "Click to Execute DataReader";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listBox1
             // 
